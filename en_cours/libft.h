6c7b434a101101a714f338912a202ac4bab886fd
Dorian Edme <dedme@z4r10p5.42lyon.fr>
2024-11-27 14:55:57 +0100

    test

---
+++ en_cours/libft.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dedme <dedme@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 12:48:34 by dedme             #+#    #+#             */
+/*   Updated: 2024/11/25 19:02:41 by dedme            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <stdlib.h>
+
+int		ft_atoi(const char *nptr);
+char	*ft_strchr(const char *s, int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_itoa(int n);
+void	bzero(void *s, size_t n);
+void	*memcpy(void *dest, const void *src, size_t n);
+
+#endif
